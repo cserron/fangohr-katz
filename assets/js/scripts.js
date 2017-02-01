a991e8e8a8ef25fc49d93286574bf92b3ff2dc30
Braulio De Leon <bradelev@hotmail.com>
2017-02-01 18:38:31 -0300

    css transition between images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,54 +32,34 @@ function cross() {
         var sectionId = $(this).closest('section');
         var sectionName = sectionId.attr('id');
         $('.menu-ul input').prop('checked', false);
-        //check the menu-item
         $('#menu-' + sectionName).prop('checked', true);
     });
 }
 
 function disableCheckbox() {
     $(".check-list [type=checkbox]").on('click', function (e) {
-        console.log('disableCheckbox');
         e.preventDefault();
     });
 }
 
-function checksOnCover() {}
-
-function menuLinks() {
-
-}
-// function carouselLinks() {
-//   var link = $('.carousel-link').data('link');
-//   var parent = link.parent('.carousel-item');
-//   parent.removeClass();
-// }
 function carouselTransition() {
-
-    // $('.carousel').on('slide.bs.carousel', function () {
-    //     // console.log($(this).attr('id'));
-    //     var carouselItem = $('.carousel-item');
-    //     var hasImage = $('.carousel-item img');
-    //     console.log('has image' + hasImage.attr('class'));
-    //     if (hasImage !== undefined) {
-    //         carouselItem.removeClass('img-filter');
-    //         carouselItem.removeClass('t2');
-    //     }
-    // });
-
-    
     $('.carousel').on('slide.bs.carousel', function () {
         var carouselItem = $(this).find('.active');
-        carouselItem.addClass('img-filter t2');
+        if (carouselItem.index() > 0) {
+            var lastCarouselItem = carouselItem.prev();
+            if (lastCarouselItem.hasClass('off')) {
+                lastCarouselItem.removeClass('off');
+            }
+        }
+        carouselItem.removeClass('off');
     });
     $('.carousel').on('slid.bs.carousel', function () {
         var carouselItem = $(this).find('.active');
-        carouselItem.removeClass('img-filter t2');
+        carouselItem.addClass('off');
     });
 }
 
 function init() {
-    console.log('init');
     scroll();
     carousel();
     cross();
