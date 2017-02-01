e5ab6ac6b553a0002f68b3085833037f2ec4d269
Braulio De Leon <bradelev@hotmail.com>
2017-02-01 16:59:01 -0300

    added image filters

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,46 +1,47 @@
 $(document).ready(init);
 
 function scroll() {
-  $(".cover-anchor").click(function() {
-    var section = $(this).parents('section');
-    $('html, body').animate({
-        scrollTop: section.next().offset().top
-    }, 1000);
-  });
+    $(".cover-anchor").click(function () {
+        var section = $(this).parents('section');
+        $('html, body').animate({
+            scrollTop: section.next().offset().top
+        }, 1000);
+    });
 }
+
 function scrollTo() {
-  $('.link-to').click(function() {
-    var target = $(this).data('target');
+    $('.link-to').click(function () {
+        var target = $(this).data('target');
 
-    $('html, body').animate({
-        scrollTop: $('#'+target).offset().top
-    }, 1000);
+        $('html, body').animate({
+            scrollTop: $('#' + target).offset().top
+        }, 1000);
 
-  });
+    });
 }
 
 function carousel() {
-  $('.carousel').carousel({
-    pause: true,
-    interval: false
-  });
+    $('.carousel').carousel({
+        pause: true,
+        interval: false
+    });
 }
 
 function cross() {
-  $(".close").click(function() {
-    var sectionId = $(this).closest('section');
-    var sectionName = sectionId.attr('id');
-    $('.menu-ul input').prop('checked', false);
-    //check the menu-item
-    $('#menu-'+sectionName).prop('checked', true);
-  });
+    $(".close").click(function () {
+        var sectionId = $(this).closest('section');
+        var sectionName = sectionId.attr('id');
+        $('.menu-ul input').prop('checked', false);
+        //check the menu-item
+        $('#menu-' + sectionName).prop('checked', true);
+    });
 }
 
 function disableCheckbox() {
-  $(".check-list [type=checkbox]").on('click', function(e) {
-    console.log('disableCheckbox');
-    e.preventDefault();
-  });
+    $(".check-list [type=checkbox]").on('click', function (e) {
+        console.log('disableCheckbox');
+        e.preventDefault();
+    });
 }
 
 function checksOnCover() {}
@@ -53,14 +54,36 @@ function menuLinks() {
 //   var parent = link.parent('.carousel-item');
 //   parent.removeClass();
 // }
+function carouselTransition() {
+
+    // $('.carousel').on('slide.bs.carousel', function () {
+    //     // console.log($(this).attr('id'));
+    //     var carouselItem = $('.carousel-item');
+    //     var hasImage = $('.carousel-item img');
+    //     console.log('has image' + hasImage.attr('class'));
+    //     if (hasImage !== undefined) {
+    //         carouselItem.removeClass('img-filter');
+    //         carouselItem.removeClass('t2');
+    //     }
+    // });
+
+    
+    $('.carousel').on('slide.bs.carousel', function () {
+        var carouselItem = $(this).find('.active');
+        carouselItem.addClass('img-filter t2');
+    });
+    $('.carousel').on('slid.bs.carousel', function () {
+        var carouselItem = $(this).find('.active');
+        carouselItem.removeClass('img-filter t2');
+    });
+}
 
 function init() {
-  console.log('init');
-  scroll();  
-  carousel();
-  cross();
-  disableCheckbox();
-  checksOnCover();
-  menuLinks();
-  scrollTo();
+    console.log('init');
+    scroll();
+    carousel();
+    cross();
+    disableCheckbox();
+    scrollTo();
+    carouselTransition();
 }
