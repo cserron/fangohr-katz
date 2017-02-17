22dee6ac19908794b198b1f1cf0330be1aa98e9e
Braulio De Leon <bradelev@hotmail.com>
2017-02-17 19:27:16 -0300

    add carousel info feature

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -129,6 +129,24 @@ function scrollFromCoverToMenu() {
     scrolled = true;
 }
 
+function toggleInfoDiv() {
+    $('.info-button').click(function() {
+        var parent = $(this).parent('.carousel-item');
+        // console.log("info-button",infoDiv.attr('class'));
+        
+        var infoDiv = parent.find('.info-div');
+        infoDiv.addClass('info-active');
+        $('.close-wrapper-info').show();
+        $(this).hide();
+    });
+    $('.close-wrapper-info').click(function() {
+        var parent = $(this).closest('.carousel-item');
+        var infoDiv = parent.find('.info-div');
+        infoDiv.removeClass('info-active');
+        parent.find('.info-button').show();
+    });
+}
+
 
 function init() {
     scroll();
@@ -141,4 +159,5 @@ function init() {
     scrollFromCoverToMenu();
     openSection();
     closeSection();
+    toggleInfoDiv();
 }
