b27e79e0bd09ead807fd8646282eac044ebd69fb
Florian Fangohr <florian@sherpaa.com>
2017-03-28 00:14:34 -0400

    debug content

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -126,12 +126,7 @@ var dic =
                 "title": "243 West End Avenue",
                 "subtitle": "New York, NY",
                 "img": "01",
-                "content": "<ul> \
-                	<li>Building Type: Residential Co-op, 183 units</li> \
-                	<li>Services Provided: Building Architect Services</li> \ 
-                </ul> \
-                <p>15-story 1925 Landmark District building designed by Emery Roth. Design and construction review services on behalf of the Board to assess proposed Shareholder renovation work for code compliance and impact on building infrastructure.
-                </p>"
+                "content": "lorem"
             },
             {
                 "title": "New York Properties",
