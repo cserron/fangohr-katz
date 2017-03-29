659fa608a1bf01c465b5007fdd5583ee35b499f7
Chris Serron <christian@bros.me>
2017-03-29 20:11:14 -0300

    checkboxes there

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,107 +1,137 @@
 $(document).ready(function () {
-    
-    
+
+    // ======================================================
+    // Main bootstrap of functions ==========================
+    // ======================================================
     initFP('.topic');
-     
-    $('.check-list').on('click',function (e) {
+
+
+    // Remove the cover
+    $('.topic-cover').click(function () {
+        $.fn.fullpage.moveSectionDown();
+        // $('.topic-cover').remove();
+        // $.fn.fullpage.reBuild;
+
+    });
+
+    // Move around the menu items
+    $('#menu .check-list').on('click', function (e) {
         e.preventDefault();
-        var topicClass =  $(this).data('target');
-       
 
-        $('.topic-cover').remove();
-        $('.topic').addClass('topic-hidden');
-        $(".menu." + topicClass).removeClass('topic-hidden');
-        $('.' + topicClass).removeClass('topic-hidden');
-        $.fn.fullpage.reBuild;
-        $.fn.fullpage.destroy('all');  
-        // $('.topic-hidden').removeClass('topic');
-        // $('.fp-completely').removeClass('fp-completely');
+        var topicClass = $(this).data('target');
+        check('.checkbox-' + topicClass);
 
-        initFP('.' + topicClass);
-        $.fn.fullpage.moveTo(2);
+        setTimeout(function () {
+            $('.topic-cover').remove();
+            $('.topic').addClass('topic-hidden');
+            $(".menu." + topicClass).removeClass('topic-hidden');
+            $('.' + topicClass).removeClass('topic-hidden');
+            $.fn.fullpage.reBuild;
+            $.fn.fullpage.destroy('all');
+
+            initFP('.' + topicClass);
+            $.fn.fullpage.moveTo(2);
+
+        }, 300);
+
+
     });
 });
 
-function initFP(topicClass='.topic') {
 
-        $('#wrapper').fullpage({
-        
+// Function to check off the checkboxes
+function check(elem) {
+
+    $(elem).each(function () {
+        console.log('elem');
+        $(this).attr('checked', true);
+        $(this).prop('checked', true);
+    });
+    console.log(elem);
+}
+
+function initFP(topicClass = '.topic') {
+
+    $('#wrapper').fullpage({
+
         //Navigation
-            // menu: '#menu',
-            lockAnchors: false,
-            anchors:['firstPage', 'secondPage'],
-            navigation: false,
-            navigationPosition: 'right',
-            navigationTooltips: ['firstSlide', 'secondSlide'],
-            showActiveTooltip: false,
-            slidesNavigation: false,
-            slidesNavPosition: 'bottom',
+        // menu: '#menu',
+        lockAnchors: false,
+        anchors: ['intro', 'home', 'building-interior', 'renovation', 'building-management'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
 
-            //Scrolling
-            css3: true,
-            scrollingSpeed: 700,
-            autoScrolling: true,
-            fitToSection: true,
-            fitToSectionDelay: 1000,
-            scrollBar: false,
-            easing: 'easeInOutCubic',
-            easingcss3: 'ease',
-            loopBottom: false,
-            loopTop: false,
-            loopHorizontal: true,
-            continuousVertical: false,
-            continuousHorizontal: false,
-            scrollHorizontally: false,
-            interlockedSlides: false,
-            dragAndMove: false,
-            offsetSections: false,
-            resetSliders: false,
-            fadingEffect: false,
-            normalScrollElements: '#element1, .element2',
-            scrollOverflow: false,
-            scrollOverflowReset: false,
-            scrollOverflowOptions: null,
-            touchSensitivity: 15,
-            normalScrollElementTouchThreshold: 5,
-            bigSectionsDestination: null,
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
 
-            //Accessibility
-            keyboardScrolling: true,
-            animateAnchor: true,
-            recordHistory: true,
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
 
-            //Design
-            controlArrows: true,
-            verticalCentered: true,
-            sectionsColor : ['#ccc', '#fff'],
-            // paddingTop: '3em',
-            // paddingBottom: '10px',
-            // fixedElements: '#menu, .footer',
-            responsiveWidth: 768,
-            responsiveHeight: 0,
-            responsiveSlides: false,
-            parallax: false,
-            parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor: ['#ccc', '#fff'],
+        // paddingTop: '3em',
+        // paddingBottom: '10px',
+        // fixedElements: '#menu, .footer',
+        responsiveWidth: 768,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+        parallax: false,
+        parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
 
-            //Custom selectors
-            sectionSelector: topicClass,
-            slideSelector: '.slide',
+        //Custom selectors
+        sectionSelector: topicClass,
+        slideSelector: '.slide',
 
-            lazyLoading: true,
+        lazyLoading: true,
 
-            //events
-            onLeave: function(index, nextIndex, direction){},
-            afterLoad: function (anchorLink, index) {
+        //events
+        onLeave: function (index, nextIndex, direction) {
+            console.log('i:', index);
+        },
+        afterLoad: function (anchorLink, index) {
 
-                // if (index == 2) {
-                //     $('.menu').addClass('fixed');
-                // }
-            },
-            afterRender: function(){},
-            afterResize: function(){},
-            afterResponsive: function(isResponsive){},
-            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-            onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { }
-            
-        });
-    }
+            // if (anchorLink === 'home') {
+            //    $('.topic-cover').addClass('topic-hidden');
+            // }
+        },
+        afterRender: function () { },
+        afterResize: function () { },
+        afterResponsive: function (isResponsive) { },
+        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) { },
+        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { }
+
+    });
+}
